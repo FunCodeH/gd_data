ae52a2f128d0e92548ca7d4bc18f4e86ddc660f0
He Wenbo <wenbo.he@dataenlighten.com>
2019-07-02 09:54:05 +0800

    日志

--- src/main/java/com/gd/data/DataApplication.java
+++ src/main/java/com/gd/data/DataApplication.java
@@ -1,29 +1,41 @@
 package com.gd.data;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("com.gd.data.mapper")
 @EnableScheduling
-public class DataApplication extends SpringBootServletInitializer {
+public class DataApplication{
 
-    public static void main(String[] args) {
-        SpringApplication.run(DataApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication application = new SpringApplication(DataApplication.class );
+        application.setApplicationContextClass( AnnotationConfigApplicationContext.class );
+        application.run( args );
+
+//        String path = System.getProperty("classpath:logback-spring.xml");
+//        PropertyConfigurator.configure(path);
+
+        CountDownLatch latch = new CountDownLatch( 1 );
+        latch.await();
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-
-        return builder.sources(DataApplication.class);
-
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//
+//        return builder.sources(DataApplication.class);
+//
+//    }
 
 }
